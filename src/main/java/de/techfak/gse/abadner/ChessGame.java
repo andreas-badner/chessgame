ed642410f54f39dcc2c564f2461be304c956ef01
abadner <abadner@techfak.uni-bielefeld.de>
2018-10-28 19:12:33 +0100

    fixed output ChessGame

--- src/main/java/de/techfak/gse/abadner/ChessGame.java
+++ src/main/java/de/techfak/gse/abadner/ChessGame.java
@@ -3,7 +3,7 @@ package de.techfak.gse.abadner;
 public class ChessGame {
 
     public static void main(final String... args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello abadner!");
     }
 
 }
