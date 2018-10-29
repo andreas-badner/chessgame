7831e8ed99dd6902b0e438d105dc8de344359d5f
abadner <abadner@techfak.uni-bielefeld.de>
2018-10-29 13:43:16 +0100

    compile, test and package done

--- src/main/java/de/techfak/gse/abadner/ChessGame.java
+++ src/main/java/de/techfak/gse/abadner/ChessGame.java
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 package de.techfak.gse.abadner;
 
 public class ChessGame {
-=======
-package de.techfak.gse.template;
-
-public class HelloWorld {
->>>>>>> uebung1
 
     public static void main(final String... args) {
         System.out.println("Hello abadner!");
