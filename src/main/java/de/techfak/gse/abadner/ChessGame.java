192f0f9a51fee000929cd663e8a3fa05e9db04dd
abadner <abadner@techfak.uni-bielefeld.de>
2018-11-17 15:58:34 +0100

    closes #52694 - done - 5 min

--- src/main/java/de/techfak/gse/abadner/ChessGame.java
+++ src/main/java/de/techfak/gse/abadner/ChessGame.java
@@ -1,13 +1,27 @@
 package de.techfak.gse.abadner;
+import java.util.Scanner;
 
+/**
+ * ChessGame Hauptklasse - Zur Ausgabe des Spielfelds unter bedingten Eingaben.
+ */
 public class ChessGame {
-
     private static final int EXIT_INTEGER = 0;
 
+    /**
+     *
+     * @param args Startparameter wird geprueft fuer die Ausgabe des Spielfelds
+     */
     public static void main(final String... args) {
         System.out.println("Hello abadner!");
         if (args.length == 0) { //Kein Startparameter
             System.out.println("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w"); //Gibt Grundstellung in FEN aus
+        }
+        Scanner reader = new Scanner(System.in);
+        String input =  reader.nextLine();
+        if (input.equals("")) {
+            System.out.println("Applikation wird beendet");
+            reader.close();
+            System.exit(EXIT_INTEGER);
         }
     }
 }
