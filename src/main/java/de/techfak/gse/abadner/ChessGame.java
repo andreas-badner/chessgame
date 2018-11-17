9bba9dda23aef05f076bdb2e006d90dddddc2ab0
abadner <abadner@techfak.uni-bielefeld.de>
2018-11-17 15:22:14 +0100

    first implementation, refs #52693

--- src/main/java/de/techfak/gse/abadner/ChessGame.java
+++ src/main/java/de/techfak/gse/abadner/ChessGame.java
@@ -1,9 +1,27 @@
 package de.techfak.gse.abadner;
+import java.util.Scanner;
 
+/**
+ * Mainclass for the ChessGame
+ */
 public class ChessGame {
 
+    private static final int EXIT_INTEGER = 100;
+
+    /**
+     *
+     * @param args Startparameter zur Ausgabe des Schachfelds
+     */
     public static void main(final String... args) {
         System.out.println("Hello abadner!");
+        if (args.length == 0) {
+            System.out.println("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w");
+        }
+        Scanner reader = new Scanner(System.in);
+        String eingabe = reader.next();
+        if (eingabe.equals(" ")) {
+            System.exit(EXIT_INTEGER);
+        }
     }
 
 }
