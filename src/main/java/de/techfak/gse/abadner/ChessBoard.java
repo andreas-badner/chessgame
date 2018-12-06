964128056f51ada7dde1177525da2e9a415caa38
Andreas Badner <abadner@techfak.uni-bielefeld.de>
2018-12-06 13:11:36 +0100

    Klasse fuer Figuren

--- src/main/java/de/techfak/gse/abadner/ChessBoard.java
+++ src/main/java/de/techfak/gse/abadner/ChessBoard.java
@@ -8,14 +8,18 @@ class ChessBoard {
 
     private static final int LAST_ARRAY_BOARD = 7;
 
-    private String[][] chessboardarr;
+    private static final String BLACK_PIECES = "prnbqk";
+
+    private static final String WHITE_PIECE = "PRNBQK";
+
+    private ChessPiece[][] chessboardarr;
 
 
     /**
      * Konstruktor der Klasse.
      */
     /* default */ChessBoard() {
-        chessboardarr = new String[ChessTurn.COLUMN_ROW_COUNT][ChessTurn.COLUMN_ROW_COUNT];
+        chessboardarr = new ChessPiece[ChessTurn.COLUMN_ROW_COUNT][ChessTurn.COLUMN_ROW_COUNT];
     }
 
     /**
@@ -24,7 +28,15 @@ class ChessBoard {
      * @param piece  Figur, die platziert werden soll oder '0'.
      */
     /* default */ void insertChessPiece(final int row, final int column, final String piece) {
-        chessboardarr[row][column] = piece;
+        String color;
+        if (BLACK_PIECES.contains(piece)) {
+            color = "black";
+        } else if (WHITE_PIECE.contains(piece)) {
+            color = "white";
+        } else {
+            color = null;
+        }
+        chessboardarr[row][column] = new ChessPiece(piece, color);
     }
 
     /**
@@ -33,7 +45,7 @@ class ChessBoard {
      * @return true wenn eine Figur auf dem Feld ist
      */
     /* default */boolean hasChessPiece(final int row, final int column) {
-        return (ChessTurn.getValidInputChars().contains(chessboardarr[row][column]));
+        return (ChessTurn.getValidInputChars().contains(ChessPiece.getBez(getChessPiece(row, column))));
     }
 
     /**
@@ -41,7 +53,7 @@ class ChessBoard {
      * @param column Position in der Spalte
      * @return gibt die Figur auf dem gesuchten Feld aus.
      */
-    /* default */String getChessPiece(final int row, final int column) {
+    /* default */ChessPiece getChessPiece(final int row, final int column) {
         return (chessboardarr[row][column]);
     }
 
@@ -53,7 +65,7 @@ class ChessBoard {
         for (int i = ChessTurn.FOR_LOOP_ZERO; i < ChessTurn.COLUMN_ROW_COUNT; i++) {
             int sum = 0;
             for (int j = ChessTurn.FOR_LOOP_ZERO; j < ChessTurn.COLUMN_ROW_COUNT; j++) {
-                if (chessboardarr[i][j].equals("1")) {
+                if (ChessPiece.getBez(getChessPiece(i,j)).equals("1")) {
                     sum++;
                     if (sum == ChessTurn.COLUMN_ROW_COUNT) {
                         stringbuilder.append(sum);
@@ -65,7 +77,7 @@ class ChessBoard {
                         stringbuilder.append(sum);
                         sum = 0;
                     }
-                    stringbuilder.append(getChessPiece(i, j));
+                    stringbuilder.append(ChessPiece.getBez(getChessPiece(i, j)));
                 }
             }
             if (i < LAST_ARRAY_BOARD) {
@@ -75,4 +87,5 @@ class ChessBoard {
         stringbuilder.append(" " + ChessTurn.getTurnOrder());
         return stringbuilder.toString();
     }
+
 }
---
+++ src/main/java/de/techfak/gse/abadner/ChessPiece.java
@@ -0,0 +1,21 @@
+package de.techfak.gse.abadner;
+
+/**
+ * Klasse, die Figurenobjekte erzeugt und deren Daten speichert.
+ */
+class ChessPiece {
+    private String bez;
+    private String color;
+    ChessPiece(String bez, String farbe) {
+        this.bez = bez;
+        this.color = farbe;
+    }
+
+    static String getBez(ChessPiece figur) {
+        return figur.bez;
+    }
+
+    String getColor(ChessPiece figur) {
+        return figur.color;
+    }
+}
